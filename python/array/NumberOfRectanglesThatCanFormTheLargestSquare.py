72c6f6c2be6d349387f1e05a3c9bf478eba3bd08
zihengCat <zihenglv@gmail.com>
2021-04-15 00:05:47 +0800

    Add Python version

---
+++ python/array/NumberOfRectanglesThatCanFormTheLargestSquare.py
@@ -0,0 +1,18 @@
+from typing import List
+
+# LeetCode 1725. Number Of Rectangles That Can Form The Largest Square
+# https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+class NumberOfRectanglesThatCanFormTheLargestSquare:
+    def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+        maxLen: int = 0
+        cnt: int = 0
+        for rec in rectangles:
+            cLen: int = min(rec[0], rec[1])
+            if (cLen > maxLen):
+                maxLen = cLen
+                cnt = 1
+            elif (cLen == maxLen):
+                cnt += 1
+        return cnt
+
+# EOF
