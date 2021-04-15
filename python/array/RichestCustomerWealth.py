795e757c2d0da93773006aefa74f69a3754c342e
zihengCat <zihenglv@gmail.com>
2021-04-15 13:25:05 +0800

    Add LeetCode 1672 Python version

---
+++ python/array/RichestCustomerWealth.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# LeetCode 1672. Richest Customer Wealth
+# https://leetcode.com/problems/richest-customer-wealth/
+class RichestCustomerWealth:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxVal: int = 0
+        for account in accounts:
+            val: int = 0
+            for n in account:
+                val += n
+            if (val > maxVal):
+                maxVal = val
+        return maxVal
+
+# EOF
