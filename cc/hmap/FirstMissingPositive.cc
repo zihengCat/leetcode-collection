26efeb7bcc782d83e5348ec91f8e3226f6e5a309
zihengCat <zihenglv@gmail.com>
2021-06-28 11:30:58 +0800

    Add LeetCode 41 (C++ version)

--- cc/hmap/FirstMissingPositive.cc
+++ cc/hmap/FirstMissingPositive.cc
@@ -1,5 +1,6 @@
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,9 +11,37 @@ using namespace std;
 class FirstMissingPositive {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        return firstMissingPositiveWithHashTable(nums);
+        // return firstMissingPositiveWithHashTable(nums);
+        return firstMissingPositiveWithSort(nums);
     }
 private:
+    /**
+     * Time complexity: O(n*log(n))
+     * Space complexity: O(1)
+     */
+    int firstMissingPositiveWithSort(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int idx = 0;
+        int vLen = nums.size();
+        for (int i = 0; i < vLen; i++) {
+            if (nums[i] > 0) { // find first non-negative integer index
+                idx = i;
+                break;
+            }
+        }
+        for (int n = 1; n < INT32_MAX; n++) {
+            if (idx == vLen) {
+                return n;
+            }
+            if (n != nums[idx]) {
+                return n;
+            }
+            while (idx < vLen && nums[idx] == n) { // skip duplicates
+                idx++;
+            }
+        }
+        return 0;
+    }
     /**
      * Time complexity: O(n)
      * Space complexity: O(n)
