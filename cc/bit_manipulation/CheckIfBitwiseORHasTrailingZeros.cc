d6a96fcb3d6d05160ad076b8b4b2daa391f833d9
zihengCat <zihenglv@gmail.com>
2024-01-11 03:01:15 +0800

    Add LeetCode 2980 (C++)

---
+++ cc/bit_manipulation/CheckIfBitwiseORHasTrailingZeros.cc
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * LeetCode 2980. Check if Bitwise OR Has Trailing Zeros
+ * https://leetcode.com/problems/check-if-bitwise-or-has-trailing-zeros/description/
+ */
+class CheckIfBitwiseORHasTrailingZeros {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return false;
+        }
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if ((nums[i] & 0x01) == 0) {
+                cnt++;
+            }
+        }
+        return cnt >= 2;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    // ...
+    return 0;
+}
+
+/* EOF */
